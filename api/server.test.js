dfa056c4a45f727d267ab4f14f7fa1ee5eaef29e
scoutreilly <scouty33@gmail.com>
2022-01-23 23:55:06 -0700

    working on tests

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,48 @@
+const request = require("supertest");
+const server = require("./server");
+
 // Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
-})
+test("sanity", () => {
+  expect(true).toBe(false);
+});
+
+describe("[POST] /register", () => {
+  it("throws an error when no username is given", async () => {
+    const res = await request(server).post("/api/auth/register").send({
+      username: "",
+      password: "foobar",
+    });
+    expect(res.body).toMatchObject({
+      message: "username and password required",
+    });
+  });
+
+  it("throws an error when no password is given", async () => {
+    const res = await request(server).post("/api/auth/register").send({
+      username: "Captain",
+      password: "",
+    });
+    expect(res.body).toMatchObject({
+      message: "username and password required",
+    });
+  });
+});
+
+describe("[POST] /login", () => {
+  it("throws an error when no username is given", async () => {
+    const res = await request(server).post("/login").send({
+      username: "",
+      password: "foobar",
+    });
+    expect(res.status).toBe(404);
+  });
+  it("throws an error when no password is given", async () => {
+    const res = await request(server).post("/api/auth/login").send({
+      username: "Captain",
+      password: "",
+    });
+    expect(res.body).toMatchObject({
+      message: "username and password required",
+    });
+  });
+});
